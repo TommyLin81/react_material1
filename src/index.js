23992521b75f2f87180452b2d887c82a1feaadb2
unknown <aaqqwwssxx77@gmail.com>
2020-06-14 16:52:17 +0800

    update Readme.md

--- src/index.js
+++ src/index.js
@@ -1,70 +1,63 @@
 import ReactDOM from 'react-dom'
 import React, { Component } from 'react'
 
-//1 area
-class Example extends Component {
+class Example1 extends Component{
+
+    render(){
+        const propties = this.props
+        console.log(propties)
+        return(
+            <div>
+                <h2>Hello React! {this.props.msg}</h2>
+                ======================================
+                <Example2 value={this.props.msg}/>
+                <Example2 value={"part 2"}/>
+                <Example2 value={"part 3"}/>
+                ======================================
+                <Example3 />
+            </div>
+        )
+    }
+}
+
+class Example2 extends Component{
+
+    render(){
+        return(
+            <div>
+                <h2>Eaxmple2 say: {this.props.value}</h2>
+            </div>
+        )
+    }
+}
+
+class Example3 extends Component{
     constructor(props){
         super(props)
         this.state ={
-            test:0
-        }
-    }
-    render() {
-        // this.props.msg2 = "Hellow 123!"
-        let string
-        return (
-            <div>
-                <h2>Hello React! {this.props.msg} {this.props.msg2}</h2>
-                ======================
-                <Example2 msg={this.props.msg}/>
-                <Example2 msg={this.props.msg2}/>
-                ======================
-                <Example3/>
-            </div>
-        )
-    }
-}
-
-//2 area
-class Example2 extends Component {
-    render() {
-        return (
-            <div>
-                <h2>Example2 say : {this.props.msg}</h2>
-            </div>
-        )
-    }
-}
-
-//3 area
-class Example3 extends Component {
-    constructor(props){
-        super(props)
-        this.state={
             number1:0
         }
-        this.number2 = 0
     }
-    addOneS=()=>{
+
+    addOne=()=>{
         this.setState({
-            number1:this.state.number1 + 1
+            number1: this.state.number1 + 1
         })
     }
-    addOneN=()=>{
-        this.number2 = this.number2 + 1
-    }
-    render() {
-        return (
+
+    render(){
+        return(
             <div>
                 <h2>Show number1 : {this.state.number1}</h2>
-                <h2>Show number2 : {this.number2}</h2>
-                <button onClick={this.addOneS}>add number1</button>
-                <button onClick={this.addOneN}>add number2</button>
+                <button onClick={this.addOne}>add </button>
             </div>
         )
     }
 }
+
+
+
 ReactDOM.render(
-    <Example msg={"Hello world!"} msg2="Hello Tommy!" />,
+    <Example1 msg={"Hello world!!"}  />,
     document.getElementById('root')
 )
